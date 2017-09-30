0980e5634e7ef0d521968c2ace27e128804f63bc
madisonflaget <maddief1996@gmail.com>
2017-09-30 09:54:36 -0600

    added hw 3 folder and edited sketch.js file

---
+++ hw-3/sketch.js
@@ -0,0 +1,17 @@
+//declare setup function
+function setup() {
+    //create a 800x600px canvas
+    createCanvas(800,600);
+
+    //set the color as 'teal'
+    background('teal');
+//end function
+}
+
+//declare draw function
+function draw() {
+    // tell it to write the following text "Hello World! I am Madison"
+    // position text 30 px in and 50 px down
+    text( "Hello World! I am Madison", 30, 50 );
+//end function
+}
