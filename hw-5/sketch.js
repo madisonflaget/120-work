1b75fd4cce96b8b50fa6c4a6838b1ebc8d16b7c2
madisonflaget <maddief1996@gmail.com>
2017-10-11 09:21:30 -0600

    added hw-5 files and set up base

---
+++ hw-5/sketch.js
@@ -0,0 +1,30 @@
+// initial variable. Define face size.
+var face_size = 100;
+
+// all other variables are determined in
+// relation to the smileySize variable.
+var eyePos_x = face_size * 0.2;
+var eyePos_y = face_size * -0.15;
+var eyeSize = face_size * 0.2;
+var face_pos_y = face_size * 0.15;
+var mouth_width = face_size * 0.75;
+var mouth_height = face_size * 0.50;
+
+function setup() {
+    // create a canvas to draw my portrait on
+    createCanvas( 1000, 600 );
+}
+
+function draw() {
+    background('gray');
+
+    //*** ANGRY FACE ***
+    push();
+
+    pop();
+
+    //*** FRUSTRATION METER ***
+    push();
+
+    pop();
+}
