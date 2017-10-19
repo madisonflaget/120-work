bd0e27398acc7b860033133eb728a9f67e881b71
Madison_Flaget <maddief1996@gmail.com>
2017-10-18 20:54:32 -0600

    make pos and size variables work

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -5,8 +5,8 @@ var rect_width = 50;
 var rect_height = 50;
 
 // declare rectangle positon variable.
-var rect_pos_x;
-var rect_pos_y;
+var rect_pos_x = width/2;
+var rect_pos_y = height/2;
 
 function setup() {
     createCanvas( windowWidth, windowHeight);
@@ -15,17 +15,17 @@ function setup() {
 
 function draw() {
 
-    // declare that the position of the rect will be randomly generated within the values, then rounded
-    // rect_pos_x = round(random( 0, rect_pos_x_con));
-    // rect_pos_y = round(random( 0, rect_pos_y_con));
-
-    // declare the the size of the rect will be randomly generated within the values, then rounded
-    // rect_width = round(random( 10, 100));
-    // rect_height = round(random( 10, 100));
-
     // draw rect
     noStroke();
     rectMode(CENTER);
     fill('blue');
-    rect( width/2, height/2, 100, 100);
+    rect( rect_pos_x, rect_pos_y, rect_width, rect_height);
+
+    // the position of the rect will be randomly generated within the values, then rounded
+    rect_pos_x = random( 0, width);
+    rect_pos_y = random( 0, height);
+
+    // the size of the rect will be randomly generated within the values, then rounded
+    rect_width = round(random( 10, 100));
+    rect_height = round(random( 10, 100));
 }
