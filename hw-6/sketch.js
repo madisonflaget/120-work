11dc492973163931ed5d377317a8281f1363baf8
Madison_Flaget <maddief1996@gmail.com>
2017-10-18 21:17:41 -0600

    added color randomization

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -1,13 +1,14 @@
 // declare object box
 var box = {};
 
-// declare variable for rectangle size
+// declare box size, position, and color qualities and random quality for color
 box.width = 50;
 box.height = 50;
-
-// declare rectangle positon variable.
 box.pos_x = width/2;
 box.pos_y = height/2;
+box.col_g = 255;
+box.col_b = 255;
+box.col_ran;
 
 function setup() {
     createCanvas( windowWidth, windowHeight);
@@ -19,8 +20,9 @@ function draw() {
     // draw rect
     noStroke();
     rectMode(CENTER);
-    fill('blue');
+    fill(0, box.col_g, box.col_b );
     rect( box.pos_x, box.pos_y, box.width, box.height);
+
 
     // the position of the rect will be randomly generated within the values, then rounded
     box.pos_x = random( 0, width);
@@ -29,4 +31,9 @@ function draw() {
     // the size of the rect will be randomly generated within the values, then rounded
     box.width = round(random( 10, 100));
     box.height = round(random( 10, 100));
+
+    //the green and blue values will be randomly generated and contrained
+    // box.col_ran =
+    box.col_g = floor(random(256));
+    box.col_b = floor(random(256));
 }
