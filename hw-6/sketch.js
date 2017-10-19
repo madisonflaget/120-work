80c44aa732db317c7abb1c2b2ab5b0b3a0f482a8
Madison_Flaget <maddief1996@gmail.com>
2017-10-18 20:46:39 -0600

    remove unnecessary variables and debugg initial code

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -1,30 +1,31 @@
-//declare variables for square
-var rect_width;
-var rect_height;
 
-// declare the contraints to be used with the positon of the rectangle
-var rect_pos_x_con = width - 20;
-var rect_pos_y_con = height - 20;
+
+// declare variable for rectangle size
+var rect_width = 50;
+var rect_height = 50;
+
+// declare rectangle positon variable.
+var rect_pos_x;
+var rect_pos_y;
 
 function setup() {
-    createCanvas( windowWidth, 600);
+    createCanvas( windowWidth, windowHeight);
     background("gray");
 }
 
 function draw() {
 
     // declare that the position of the rect will be randomly generated within the values, then rounded
-    var rect_pos_x = round(random( 0, rect_pos_x_con));
-    var rect_pos_y = round(random( 0, rect_pos_y_con));
+    // rect_pos_x = round(random( 0, rect_pos_x_con));
+    // rect_pos_y = round(random( 0, rect_pos_y_con));
 
     // declare the the size of the rect will be randomly generated within the values, then rounded
-    var rect_width = round(random( 10, 100));
-    var rect_height = round(random( 10, 100));
+    // rect_width = round(random( 10, 100));
+    // rect_height = round(random( 10, 100));
 
     // draw rect
     noStroke();
     rectMode(CENTER);
     fill('blue');
-    rect( rect_pos_x, rect_pos_y, rect_width, rect_height);
-
+    rect( width/2, height/2, 100, 100);
 }
