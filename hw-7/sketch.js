6710f7a5d31680c516972f47a06e4956422b2a06
Madison_Flaget <maddief1996@gmail.com>
2017-10-26 19:26:53 -0600

    attempting to make my changes, with limited success

--- hw-7/sketch.js
+++ hw-7/sketch.js
@@ -8,6 +8,7 @@ ball.delta_x = 1;
 ball.delta_y = 1;
 ball.scale_x = 1;
 ball.scale_y = 1;
+ball.color = 'rgba( 255, 255, 255, 0.5)';
 
 function setup() {
     createCanvas(windowWidth, 400);
@@ -21,19 +22,37 @@ function draw() {
     ball.x += ball.delta_x * ball.scale_x;
     ball.y += ball.delta_y * ball.scale_y;
 
-
+//** X AXIS **************************************
     if (ball.x >= width || ball.x <= 0) {
         ball.delta_x = -1 * ball.delta_x;
+        ball.color = 'rgba( 255, 0, 0, 1 )';
+
+    } else {
+        ball.color = 'rgba( 100, 200, 100, 0.3 )';
     }
+//** END **************************************
+
+//** Y AXIS ************************************
     if (ball.y >= height || ball.y <= 0) {
         ball.delta_y = -1 * ball.delta_y;
-    }
+        ball.color = 'rgba( 255, 0, 0, 1 )';
 
-    fill(255);
+    } else {
+        ball.color = 'rgba( 100, 200, 100, 0.3 )';
+    }
+//** END ****************************************
+
+    // if (mouseIsPressed){
+    //     ball.color = 'rgba( random(255), random(255), random(255), 0.5)';
+    // }
+
+    fill(ball.color);
     ellipse(ball.x, ball.y, ball.width, ball.width);
 }
 
 function mousePressed() {
     ball.scale_x = map(mouseX, 0, width, 0.5, 10);
     ball.scale_y = map(mouseY, 0, height, 0.5, 10);
+    ball.color = random(255), random(255), random(255);
+    background(random(255), random(255), random(255));
 }
